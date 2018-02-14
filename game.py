b75398064961fe8535f8ff92c6ff47e244e029c3
emanayman657 <36210449+emanayman657@users.noreply.github.com>
2018-02-14 22:16:44 +0200

    Add files via upload

---
+++ game.py
@@ -0,0 +1,23 @@
+z=0
+x=0
+y=0
+while z<=100:
+    if z==100:
+        print("you win")
+        break
+    else:
+        x=int(input("enter the number player 1:"))
+        if x<=10:
+            z=x+z
+            if z>100:
+                print("error")
+                break
+        else:
+                print("error")
+                break
+        y=int(input("enter the number player 2 :"))
+        if y<=10:
+                        z=z+y
+        else:
+                        print("error")
+                        break
